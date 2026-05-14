c096484cfe759ee153f39c9a045da9dc4c5cdd68
hartaimonikavalentina-design <hartaimonikavalentina@gmail.com>
2026-05-14 09:27:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,6 @@ st.set_page_config(page_title="Meteorológiai projektmunka", layout="wide")
 st.title("Meteorológiai idősorok – projektmunka")
 st.write("Hartai Mónika Valentina – Script nyelvek beadandó")
 
-st.markdown("""
-### Bevezetés
-
-Ez egy Streamlit alapú webes felület, amely a meteorológiai adatok vizsgálatára készült.
-A következő lépésekben ide fogjuk beépíteni:
-- az adatbeolvasást,
-- a grafikonokat,
-- az interaktív csúszkákat és választólistákat,
-- valamint a projektmunka szöveges fejezeteit.
-""")
 
 import pandas as pd
 
